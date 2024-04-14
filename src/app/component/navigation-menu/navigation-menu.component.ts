212f580642b7ffcae5492f3b31e6385e433eb571
Guti <gutifer666@gmail.com>
2024-04-14 21:35:35 +0200

    added navigation menu

---
+++ src/app/component/navigation-menu/navigation-menu.component.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { AsyncPipe, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-navigation-menu',
+  templateUrl: './navigation-menu.component.html',
+  styleUrls: ['./navigation-menu.component.css']
+})
+export class NavigationMenuComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+}
